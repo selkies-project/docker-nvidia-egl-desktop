de6e31af57e0dbb607132eca2355ecc95c82f639
Restyled.io <commits@restyled.io>
2020-12-22 10:00:26 +0000

    Restyled by shellharden

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,13 +3,13 @@ set -e
 
 trap "echo TRAPed signal" HUP INT QUIT KILL TERM
 
-export PATH=${PATH}:/opt/VirtualGL/bin:/opt/TurboVNC/bin
+export PATH="$PATH":/opt/VirtualGL/bin:/opt/TurboVNC/bin
 
 mkdir -p ~/.vnc
-echo $VNCPASS | vncpasswd -f > ~/.vnc/passwd
+echo "$VNCPASS" | vncpasswd -f > ~/.vnc/passwd
 chmod 0600 ~/.vnc/passwd
 
-if [ "x${SHARED}" == "xTRUE" ]; then
+if [ "x$SHARED" == "xTRUE" ]; then
     export SHARESTRING="-shared"
 fi
 
@@ -17,15 +17,15 @@ printf "3\nn\nx\n" | sudo /opt/VirtualGL/bin/vglserver_config
 
 for drm in /dev/dri/card*
 do
-if /opt/VirtualGL/bin/eglinfo ${drm}; then
-    export VGL_DISPLAY=${drm}
+if /opt/VirtualGL/bin/eglinfo "$drm"; then
+    export VGL_DISPLAY="$drm"
     break
 fi
 done
 
 export TVNC_WM=mate-session
 
-/opt/websockify/run 5901 --web=/opt/noVNC --wrap-mode=ignore -- vncserver :1 -geometry $SIZEW"x"$SIZEH -depth $CDEPTH -vgl &
+/opt/websockify/run 5901 --web=/opt/noVNC --wrap-mode=ignore -- vncserver :1 -geometry "${SIZEW}x""$SIZEH" -depth "$CDEPTH" -vgl &
 
 gsettings set org.gnome.desktop.lockdown disable-lock-screen true
 gsettings set org.gnome.desktop.screensaver lock-enabled false
